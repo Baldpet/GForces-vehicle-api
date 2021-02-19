3a6dcac248d6fb36b218d3500dd4e5c788c33eac
Baldpet <peterdaniel.baldwin@gmail.com>
2021-02-19 18:06:03 +0000

    Fixed an error on the update function

--- app.py
+++ app.py
@@ -60,27 +60,20 @@ def create():
 @app.route('/api/update/<order_number>', methods=['POST'])
 def update(order_number):
     try:
-        if 'vehicle_manufacturer' in request.args:
-            vehicle_manufacturer = request.args['vehicle_manufacturer']
-        else:
-            vehicle_manufacturer = None
-        if 'model' in request.args:
-            model = request.args['model']
-        else:
-            model = None
-        if 'total_price' in request.args:
-            total_price = request.args['total_price']
-        else:
-            total_price = None
-
         order = mongo.db.orders
 
-        order.update_one({'order_number': order_number},
-                        {'$set': {'vehicle_manufacturer': vehicle_manufacturer,
-                                'model': model,
-                                'total_price': total_price
-                                }
-                        })
+        if 'vehicle_manufacturer' in request.args:
+            vehicle_manufacturer = request.args['vehicle_manufacturer']
+            order.update_one({'order_number': order_number},
+                             {'$set': {'vehicle_manufacturer': vehicle_manufacturer}})
+        if 'model' in request.args:
+            model = request.args['model']
+            order.update_one({'order_number': order_number},
+                             {'$set': {'model': model}})
+        if 'total_price' in request.args:
+            total_price = request.args['total_price']
+            order.update_one({'order_number': order_number},
+                             {'$set': {'total_price': total_price}})
 
         return Response(status=200)
     except:
